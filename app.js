f516c4585ffa7650870fcc8aeca971164af6e769
Abdul Rafay <rafay0704@gmail.com>
2025-08-30 15:05:21 +0400

    subscription tracker

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(arcjetMiddleware);
+// app.use(arcjetMiddleware);
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
